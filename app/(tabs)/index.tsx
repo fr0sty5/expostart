4bfd46fbb614b304e7c46df2b66dd11115c6210e
Emanuele <francescobag007@gmail.com>
2024-05-22 13:37:19 +0200

    home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Pressable, View, ScrollView, useColorScheme } from 'react-native';
+import { Image, StyleSheet, Pressable, View, ScrollView, useColorScheme, FlatList } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { useRouter } from 'expo-router';
 import React, { useEffect, useState } from 'react';
@@ -57,7 +57,7 @@ export default function HomeScreen() {
 
   const [listFilmGenre, setlistFilmGenre] = useState<any>(null)
 
-  function ListFilm (id: string){
+  function ListFilm(id: string) {
     const options = {
       method: 'GET',
       headers: {
@@ -65,14 +65,12 @@ export default function HomeScreen() {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODdkMTNiZjIyN2IxM2Q3NWQ3Mjk2OTY0NjQ1OGZiMiIsInN1YiI6IjY2NGM0NzliYjMxYTg1YjNiNTY2OWYxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jWmBf7QPdrNFVME02-nRhg6NcDUd7Wv4NVUX80EGzRE'
       }
     };
-    
-    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=it-IT&page=1&sort_by=popularity.desc&with_genres='+id, options)
+
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=it-IT&page=1&sort_by=popularity.desc&with_genres=' + id, options)
       .then(response => response.json())
       .then(response => setlistFilmGenre(response))
       .catch(err => console.error(err));
   }
-
-  console.log(listFilmGenre?.results)
 
   return (
     <SafeAreaView style={styles.Maincontainer}>
@@ -94,32 +92,30 @@ export default function HomeScreen() {
         ))}
       </ScrollView>
 
-      <ScrollView horizontal>
-        {genre?.genres.map((genere: any) => (
-          <Pressable style={[{
-            margin: 10,
-            padding: 5,
-          }]} onPress={() => ListFilm(genere.id)}>
-            <ThemedText>{genere.name}</ThemedText>
-          </Pressable>
-        ))}
-      </ScrollView>
+      <FlatList
+        horizontal={true}
+        data={genre?.genres}
+        renderItem={({ item }) => <Pressable onPress={() => ListFilm(item.id)}>
+          <ThemedText style={styles.listaGeneri}>{item.name}</ThemedText>
+        </Pressable>}
+      keyExtractor={item => item.id}
+      />
 
       <ScrollView horizontal>
         {listFilmGenre?.results.map((filmGenere: any) => (
           <Pressable key={filmGenere.id} onPress={() => goToDetail(filmGenere.id)}>
-          <View style={styles.container_img}>
-            <Image
-              style={styles.img}
-              source={{
-                uri: 'http://image.tmdb.org/t/p/w500/' + filmGenere.poster_path,
-              }} />
-          </View>
-          <ThemedText style={styles.filmTitle} numberOfLines={1}>{filmGenere.original_title}</ThemedText>
-        </Pressable>
+            <View style={styles.container_img}>
+              <Image
+                style={styles.img}
+                source={{
+                  uri: 'http://image.tmdb.org/t/p/w500/' + filmGenere.poster_path,
+                }} />
+            </View>
+            <ThemedText style={styles.filmTitle} numberOfLines={1}>{filmGenere.original_title}</ThemedText>
+          </Pressable>
         ))}
       </ScrollView>
-      
+
     </SafeAreaView>
   );
 }
@@ -161,5 +157,12 @@ const styles = StyleSheet.create({
   filmTitle: {
     width: 120,
     padding: 5,
+  },
+  listaGeneri: {
+    margin: 10,
+    padding: 5,
+  },
+  listaGeneriOnPress: {
+
   },
 });
